f590628e17babfa973d5f9ae697247789d220ea8
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2022-04-10 00:16:04 +0900

    実装開始

---
+++ src/components/Board.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- ここから追加 -->
+  <div>
+    <header>
+      my Trello
+    </header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+    </main>
+  </div>
+  <!-- ここまで追加 -->
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
